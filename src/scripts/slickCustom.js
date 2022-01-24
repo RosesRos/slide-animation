b0336f3bc38e9dcdc5b35e6fde00894ea44c29a9
FABIO214-BIT <rostranespinoza@gmail.com>
2022-01-24 19:19:04 +0300

    files_project

--- src/scripts/slickCustom.js
+++ src/scripts/slickCustom.js
@@ -10,60 +10,11 @@ $(function ($) {
         infinite: true,
         speed: 500,
         draggable: false,
-        //prevArrow: '<img class="slider-arrow slider-arrow__left" src="./assets/arrow_left.svg">',
-        //nextArrow: '<img class="slider-arrow slider-arrow__right" src="./assets/arrow_right.svg">',
         autoplay: true,
         autoplaySpeed: 8000,
         fade: !0,
         cssEase: 'linear',
         slidesToShow: 1,
         slidesToScroll: 1,
-        /*responsive: [
-            {
-                breakpoint: 1100,
-                settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 1,
-                    dots: false,
-                    arrows: true,
-                }
-            },
-            {
-                breakpoint: 850,
-                settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 1,
-                    dots: false,
-                    arrows: true,
-                }
-            },
-            {
-                breakpoint: 770,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 1,
-                    dots: false,
-                    arrows: true,
-                }
-            },
-            {
-                breakpoint: 650,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 1,
-                    dots: false,
-                    arrows: true,
-                }
-            },
-            {
-                breakpoint: 480,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1,
-                    dots: false,
-                    arrows: true,
-                }
-            },
-        ]*/
     });
 });
